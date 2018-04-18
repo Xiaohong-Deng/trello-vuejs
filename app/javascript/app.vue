5b20a20e4c59b1a2e43a43457e10869a782a8020
Xiaohong-Deng <worldsky86rough@gmail.com>
2018-04-18 17:06:54 +0800

    finish ep1

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app" class="row">
-    <div v-for="list in original_lists" class=""col-3">
-    <h6>{{ list.name }}</h6>
-    <hr/>
-    <div v-for="(card, index) in list.cards" class="card card-body">
-      {{ card.name }}
-    </div>
+    <div v-for="list in original_lists" class="col-3">
+      <h6>{{ list.name }}</h6>
+      <hr/>
+      <div v-for="(card, index) in list.cards" class="card card-body">
+        {{ card.name }}
+      </div>
     </div>
   </div>
 </template>
